884d9af1a95a292cf5b5b473a3e41b9e7c54b6e0
YumeYuka <yumeyuka@oom-wg.dev>
2025-07-05 22:09:40 +0800

    Add fullScreen function and update component formatting
    Signed-off-by: YumeYuka <yumeyuka@oom-wg.dev>

--- src/types/kernelsu.d.ts
+++ src/types/kernelsu.d.ts
@@ -1,9 +1,13 @@
 declare module "kernelsu" {
-  export interface ExecResult {
-    errno: number;
-    stdout: string;
-    stderr: string;
-  }
-  export function exec(command: string, options?: any): Promise<ExecResult>;
-  export function toast(message: string): void;
+    export interface ExecResult {
+        errno: number;
+        stdout: string;
+        stderr: string;
+    }
+
+    export function exec(command: string, options?: any): Promise<ExecResult>;
+
+    export function toast(message: string): void;
+
+    export function fullScreen(isFullScreen: boolean): void;
 }
